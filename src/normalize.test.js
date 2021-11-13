297c92b5b0756ab51854920535210d13f46afb87
Oliver Tupman <oliver@dts-workshop.com>
2021-11-13 10:22:17 +0000

    Adds initial test case

---
+++ src/normalize.test.js
@@ -0,0 +1,37 @@
+import { buildCustomSchema } from './normalize';
+
+describe('buildCustomSchema', () => {
+  const schema = {} // Schema from CS
+  const parent = null;
+  const prefix = 'prefix';
+  const disableMandatoryFields = false;
+
+  const build = schema => {
+    return buildCustomSchema(schema, null, null, null, null, parent, prefix, disableMandatoryFields);
+  }
+
+  describe('text field', () => {
+    const stringField = opts => {
+      return {
+        uid: 'jestblt1010101',
+        data_type: 'text',
+        mandatory: true,
+        multiple: true,
+        ...opts,
+      }
+    }
+
+    it.each([
+      { fieldFn: stringField, fieldOpts: { mandatory: true, multiple: true }, expectedType: '[String]!' },
+      { fieldFn: stringField, fieldOpts: { mandatory: true, multiple: false }, expectedType: 'String!' },
+    ])
+    ( 'built $fieldOpts must be type $expectedType', ({ fieldFn, fieldOpts, expectedType }) => {
+      const testField = fieldFn(fieldOpts);
+      const builtFields = build([testField]);
+      const builtField = builtFields.fields[testField.uid]
+      expect(builtField).toMatchObject({ type: expectedType });
+    });
+
+
+  })
+})
