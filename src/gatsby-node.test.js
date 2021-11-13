3890eceab79276764d566d76fcec73ab85101e23
Oliver Tupman <oliver@dts-workshop.com>
2021-11-13 16:32:35 +0000

    Fixes test module mocking

--- src/gatsby-node.test.js
+++ src/gatsby-node.test.js
@@ -1,45 +1,46 @@
 
+jest.doMock('gatsby-plugin-utils', () => ({
+  isGatsbyNodeLifecycleSupported: () => false,
+}))
+
+jest.mock('./fetch', () => ({
+  fetchData: () => {
+    return new Promise((resolve) => {
+      resolve({})
+    })
+  },
+  fetchContentTypes: () => new Promise((resolve) => {
+    resolve([])
+  }),
+}))
+
 let createSchemaCustomization = null;
+let normalize = null;
 
 describe('createSchemaCustomization', () => {
-  beforeEach(() => {
+  beforeEach( () => {
     jest.resetModules();
-    // TODO: figure out how to mock the package.json
-    // jest.mock('./package.json', () => ([1, 2, 3]));
 
-    jest.doMock('gatsby-plugin-utils', () => ({
-      isGatsbyNodeLifecycleSupported: () => false,
-    }))
-    jest.mock('./fetch', () => ({
-      fetchData: () => {
-        return new Promise((resolve) => {
-          resolve({})
-        })
-      },
-      fetchContentTypes: () => new Promise((resolve) => {
-        resolve([])
-      }),
-    }))
     jest.mock('./normalize', () => ({
       extendSchemaWithDefaultEntryFields: () => {},
-      buildCustomSchema: () => ({
+      buildCustomSchema: jest.fn().mockImplementation(() => ({
         references: [], groups: [], fileFields: [], types: [],
-      }),
+      }))
     }))
 
-    const { createSchemaCustomization: createSchemaCustomizationOrig,  } = require('./gatsby-node')
-
-    createSchemaCustomization = createSchemaCustomizationOrig;
-  });
+    const { createSchemaCustomization: createSchemaCustomisationOrig  } = require('./gatsby-node')
+    createSchemaCustomization = createSchemaCustomisationOrig;
+    normalize = require('./normalize');
+  })
 
   describe.each([
     [{ enableSchemaGeneration: true, }, 1],
     [{ enableSchemaGeneration: false, }, 0]
   ])(`with config %p is called %i times`, (pluginConfig, callCount) => {
     it('calls the customisation function if needed', async () => {
-        const createTypesMock = jest.fn();
+      const createTypesMock = jest.fn();
 
-        await createSchemaCustomization(
+      await createSchemaCustomization(
         {
           cache: {set: () => {}},
           actions: { createTypes: createTypesMock, },
